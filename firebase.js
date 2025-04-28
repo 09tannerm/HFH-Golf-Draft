076bbe2bf9cd66b848e39a464792b77bb722e484
09tannerm <09tannerm@gmail.com>
2025-04-28 17:35:35 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "YOUR-PROJECT-ID.firebaseapp.com",
-  projectId: "YOUR-PROJECT-ID",
-  storageBucket: "YOUR-PROJECT-ID.appspot.com",
-  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
-  appId: "YOUR-APP-ID"
+  apiKey: "", // no Web API key available yet
+  authDomain: "hfh-golf-draft.firebaseapp.com",
+  projectId: "hfh-golf-draft",
+  storageBucket: "hfh-golf-draft.appspot.com",
+  messagingSenderId: "88207474267",
+  appId: "" // add appId here if available
 };
 
 const app = initializeApp(firebaseConfig);
